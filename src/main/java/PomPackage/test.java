4efb20fb7d6cdd401dd6d142d3d7da54b609dfbb
Amitautomation <amitborude1@gmail.com>
2023-03-28 16:17:09 +0530

    m2 added

--- src/main/java/PomPackage/test.java
+++ src/main/java/PomPackage/test.java
@@ -3,5 +3,12 @@ package PomPackage;
 public class test {
 public void m1() {
 	System.out.println("hiiiiii");
+	
+	System .out.println("hello world");
+}
+public void m2() {
+	System.out.println("buy");
+	
+	System .out.println("buuuu");
 }
 }
